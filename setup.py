f0cb598d372bedfc402164c12a57faa61a2032ce
George Richardson <georgerichardson@posteo.net>
2019-01-14 12:57:19 +0000

    add pdf_cdf

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='eurito_funding',
     packages=find_packages(),
     version='0.1.0',
     description='EURITO: Advanced R&I funding analytics pilot',
