db41883eb468a5cb50d9dd1c531b6ad8fa04f2a6
George Richardson <georgerichardson@posteo.net>
2019-02-12 15:53:28 +0000

    add mapping generator

--- eu_funding/utils/misc_utils.py
+++ eu_funding/utils/misc_utils.py
@@ -13,3 +13,18 @@ def print_nested_structure(j, level=0):
         elif (v is not None) & (type(v) != str):
             if isinstance(v[1], dict):
                 print_nested_structure(v[0], level=level+1)
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def generate_mapping(df, col_1, col_2):
+    mapping = {}
+    for a, b in zip(df[col_1], df[col_2]):
+        if a in mapping:
+            continue
+        else:
+            if ~pd.isnull(b):
+                mapping[a] = b
+    return mapping
