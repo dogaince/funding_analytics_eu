42fad368a90af2d0530ad83ac5c443c04fda1c73
George Richardson <georgerichardson@posteo.net>
2019-02-27 00:22:44 +0000

    k factor and linguistic similarity

--- eu_funding/utils/nlp_utils.py
+++ eu_funding/utils/nlp_utils.py
@@ -4,13 +4,24 @@ from gensim.models.phrases import Phrases, Phraser
 
 
 def remove_markup(text):
-    tags = [
-        '<b>', '<p>', '&nbsp;', '<li>', '<ol>', '<ul>', '<br>',
-        '</b>', '</p>', '</li>', '</ol>', '</ul>', '</br>',
-        '\n', '\t', '\r'
-    ]
+#     tags = [
+#         '<b>', '<p>', '&nbsp;', '<li>', '<ol>', '<ul>', '<br>',
+#         '</b>', '</p>', '</li>', '</ol>', '</ul>', '</br>',
+#         '\n', '\t', '\r'
+#     ]
+    tags = ["a","abbr","acronym","address","area","b","base","bdo","big",
+            "blockquote","body","br","button","caption","cite","code","col",
+            "colgroup","dd","del","dfn","div","dl","DOCTYPE","dt","em","fieldset",
+            "form","h1","h2","h3","h4","h5","h6","head","html","hr","i","img",
+            "input","ins","kbd","label","legend","li","link","map","meta",
+            "noscript","object","ol","optgroup","option","p","param","pre","q",
+            "samp","script","select","small","span","strong","style","sub",
+            "sup","table","tbody","td","textarea","tfoot","th","thead",
+            "title","tr","tt","ul","var"]
+    
     for tag in tags:
-        text = text.replace(tag, ' ')
+        text = text.replace('<{}>'.format(tag), ' ')
+        text = text.replace('</{}>'.format(tag), ' ')
     return text
 
 def normalise_digits(text):
